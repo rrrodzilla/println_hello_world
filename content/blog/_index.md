979ddeabac76d84ca4853b8d2218636204f4e52f
rodzilla <rolandrodriguez@gmail.com>
2021-08-06 18:59:45 -0500

    html and design formatting

--- content/blog/_index.md
+++ content/blog/_index.md
@@ -1,5 +1,5 @@
 +++
-title = "Recent posts"
+title = "Latest posts"
 sort_by = "date"
 template = "blog.html"
 page_template = "blog-page.html"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   purge: ["./templates/**/*.html", "./theme/**/*.html"],
-  theme: {},
+  theme: {
+extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
   variants: {},
   plugins: [
       require('@tailwindcss/typography'),
